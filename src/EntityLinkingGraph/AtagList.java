81fed0332c1e08001d17bbe1e047546b58a3d0bd
Hossein Aghahosseini <h.aghahosseini1996@gmail.com>
2018-05-23 23:39:16 +0430

    Single page <a> tag extraction completed

--- src/EntityLinkingGraph/AtagList.java
+++ src/EntityLinkingGraph/AtagList.java
@@ -1,8 +1,9 @@
 
 package EntityLinkingGraph;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 /**
  * @author hosseinAghahosseini
@@ -10,13 +11,123 @@ import java.util.Scanner;
 
 public class AtagList {
     
-    public String link = "";
+    public String linkURL = "";
+    public Document doc;
+    public Elements links;
     
-    public ArrayList<Atag> allTags = new ArrayList<>();
-    
-    public void initialize (String pageData)
+    public void initialize (String url , Boolean includeContent)
     {
-        Scanner input = new Scanner("");
+        linkURL = url;
+        htmlPageGetter a = new htmlPageGetter();
         
+        int referencesIndex = 0;
+        String referencesRemoved = a.getHTML(url);
+        
+        //content removal  - <h2>Contents</h2>
+        if(includeContent == false)
+        {
+            //referencesIndex = 4;
+            referencesIndex = referencesRemoved.indexOf("<h2>Contents</h2>");
+            //System.out.println("before:"+referencesRemoved);
+            System.out.println(referencesIndex);
+            referencesRemoved = referencesRemoved.substring(0, referencesIndex-1);
+            //System.out.println("after:"+referencesRemoved);
+        }
+        //System.out.print(referencesRemoved);
+        else {
+        //references removal
+            try 
+            {
+                referencesIndex = referencesRemoved.indexOf("<span class=\"mw-headline\" id=\"References\">References</span>");
+                referencesRemoved = referencesRemoved.substring(0, referencesIndex-1);
+            }
+            catch (StringIndexOutOfBoundsException se) 
+            {
+                referencesRemoved = a.getHTML(url);
+            }
+        }
+        
+        //info table removal 
+        String tempR = referencesRemoved;
+        try {
+            referencesIndex = referencesRemoved.indexOf("<table class=\"infobox");
+            for(int i = referencesIndex ; i < referencesRemoved.length()-1 ; i++ )
+            {
+                if(referencesRemoved.substring(i, i+8).equals("</table>"))
+                {
+                    //System.out.println(referencesRemoved.substring(i, i+8));
+                    String temp = "";
+                    temp = referencesRemoved.substring(0, referencesIndex+1);
+                    temp += referencesRemoved.substring(i+8, referencesRemoved.length()-1);
+                    referencesRemoved = temp;
+                    break;
+                }
+            }
+        }
+        catch (StringIndexOutOfBoundsException se) 
+        {
+            referencesRemoved = tempR;
+        }
+        
+        //<div class="thumb removal
+        tempR = referencesRemoved;
+        try {
+            referencesIndex = referencesRemoved.indexOf("<div class=\"thumb");
+            for(int i = referencesIndex ; i < referencesRemoved.length()-1 ; i++ )
+            {
+                if(referencesRemoved.substring(i, i+6).equals("</div>"))
+                {
+                    //System.out.println(referencesRemoved.substring(i, i+8));
+                    String temp = "";
+                    temp = referencesRemoved.substring(0, referencesIndex+1);
+                    temp += referencesRemoved.substring(i+6, referencesRemoved.length()-1);
+                    referencesRemoved = temp;
+                    break;
+                }
+            }
+        }
+        catch (StringIndexOutOfBoundsException se) 
+        {
+            referencesRemoved = tempR;
+        }
+                
+//        referencesIndex = referencesRemoved.indexOf("<div class=\"thumb");
+//        //System.out.print(referencesIndex);
+//        if(referencesIndex >= 0)
+//        {
+//            referencesRemoved = referencesRemoved.substring(0, referencesIndex-1);
+//        }
+//        
+        
+        doc = Jsoup.parse(referencesRemoved);
+        links = doc.select("a[href]");
+    }
+    
+    public void remove_Hashtag()
+    {
+        //try 
+        {
+            for (int i = 0 ; i < links.size() ; i++)
+            {   
+                //hashtag
+                if(links.get(i).attr("href").charAt(0) == '#')
+                {
+                    links.remove(i);
+                    i--;
+                }
+                
+                /*img
+                if(i>=0 && links.get(i).toString().contains("<img"))
+                {
+                    links.remove(i);
+                    i--;
+                }
+                */
+            }
+        }
+        //catch (Exception er)
+        {
+            
+        }
     }
 }
--- src/EntityLinkingGraph/EntityLinkigGraph.java
+++ src/EntityLinkingGraph/EntityLinkigGraph.java
@@ -1,29 +1,35 @@
 
 package EntityLinkingGraph;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 /**
  * @author hosseinAghahosseini
  */
 
-import java.io.*;
-import java.net.*;
-
 public class EntityLinkigGraph {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println(LocalDateTime.now());
+        
         htmlPageGetter a = new htmlPageGetter();
-        try 
-        {
-            //System.out.println(a.getHTML("https://www.google.com/"));
-            
-        }
-        catch (Exception e1) {
-            System.out.println(e1.getMessage());
-        }
+        //System.out.println(a.getHTML("https://www.google.com/"));
+        //String url = "https://en.wikipedia.org/wiki/Louis_V._Gerstner_Jr.";
+        //String url = "https://en.wikipedia.org/wiki/Alessandro_Del_Piero";
+        String url = "https://en.wikipedia.org/wiki/Graph_(discrete_mathematics)";
+        
+        AtagList lst = new AtagList();
+        lst.initialize(url,false);
+        
+        lst.remove_Hashtag();
+        
+        for (int i = 0 ; i < lst.links.size() ; i++)
+            System.out.println(lst.links.get(i).attr("href"));
+        
+        System.out.println(LocalDateTime.now());
+
     }
     
 }
